b3ac2ce2353e1dd48ca58d3433ffae15fc18ab58
Id said Salman <salmanartist@03gmail.com>
2023-08-28 12:01:51 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import AboutMe from "./components/AboutMe";
+import Myskills from "./components/Myskills";
+import MyProjects from "./components/MyProjects";
+import ContactMe from "./components/ContactMe";
+import SchoolTimeline from "./components/SchoolTimeline";
+import Experience from "./components/Experience";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <AboutMe />
+      <SchoolTimeline/>
+      
+      <Myskills />
+      <MyProjects />
+      <ContactMe />
     </div>
   );
-}
+};
 
 export default App;
